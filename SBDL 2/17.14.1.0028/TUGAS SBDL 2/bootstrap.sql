53a0f4ea324428e2b251a7859bbf8ebfc326fc39
mochamad amran al fariz <49631648+amrand24@users.noreply.github.com>
2019-06-21 15:51:46 +0700

    penambahan view

--- SBDL 2/17.14.1.0028/TUGAS SBDL 2/bootstrap.sql
+++ SBDL 2/17.14.1.0028/TUGAS SBDL 2/bootstrap.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 09 Bulan Mei 2019 pada 12.09
--- Versi server: 10.1.37-MariaDB
--- Versi PHP: 7.3.0
+-- Generation Time: Jun 21, 2019 at 09:24 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `guru`
+-- Table structure for table `guru`
 --
 
 CREATE TABLE `guru` (
@@ -37,7 +37,7 @@ CREATE TABLE `guru` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `guru`
+-- Dumping data for table `guru`
 --
 
 INSERT INTO `guru` (`idguru`, `nip`, `nama_guru`, `jenis_kelamin`, `alamat`) VALUES
@@ -55,7 +55,7 @@ INSERT INTO `guru` (`idguru`, `nip`, `nama_guru`, `jenis_kelamin`, `alamat`) VAL
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kelas`
+-- Table structure for table `kelas`
 --
 
 CREATE TABLE `kelas` (
@@ -65,7 +65,7 @@ CREATE TABLE `kelas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `kelas`
+-- Dumping data for table `kelas`
 --
 
 INSERT INTO `kelas` (`idkelas`, `nama_kelas`, `idguru`) VALUES
@@ -83,10 +83,10 @@ INSERT INTO `kelas` (`idkelas`, `nama_kelas`, `idguru`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `mata pelajaran`
+-- Table structure for table `mapel`
 --
 
-CREATE TABLE `mata pelajaran` (
+CREATE TABLE `mapel` (
   `idmatapelajaran` int(11) NOT NULL,
   `nama_mata_pelajaran` varchar(50) NOT NULL,
   `idguru` int(11) NOT NULL,
@@ -94,10 +94,10 @@ CREATE TABLE `mata pelajaran` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `mata pelajaran`
+-- Dumping data for table `mapel`
 --
 
-INSERT INTO `mata pelajaran` (`idmatapelajaran`, `nama_mata_pelajaran`, `idguru`, `idnilai`) VALUES
+INSERT INTO `mapel` (`idmatapelajaran`, `nama_mata_pelajaran`, `idguru`, `idnilai`) VALUES
 (200, 'PKN', 10, 50),
 (300, 'MTK', 20, 51),
 (400, 'IPS', 30, 52),
@@ -112,34 +112,56 @@ INSERT INTO `mata pelajaran` (`idmatapelajaran`, `nama_mata_pelajaran`, `idguru`
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `nilai`
+-- Table structure for table `nilai`
 --
 
 CREATE TABLE `nilai` (
   `idnilai` int(11) NOT NULL,
-  `idsiswa` int(11) NOT NULL
+  `idsiswa` int(11) NOT NULL,
+  `nilai` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `nilai`
+-- Dumping data for table `nilai`
 --
 
-INSERT INTO `nilai` (`idnilai`, `idsiswa`) VALUES
-(60, 1000),
-(61, 1001),
-(62, 1002),
-(63, 1003),
-(64, 1004),
-(65, 1005),
-(66, 1006),
-(67, 1007),
-(68, 1008),
-(69, 1009);
+INSERT INTO `nilai` (`idnilai`, `idsiswa`, `nilai`) VALUES
+(60, 1000, 80),
+(61, 1001, 80),
+(62, 1002, 75),
+(63, 1003, 75),
+(64, 1004, 85),
+(65, 1005, 70),
+(66, 1006, 90),
+(67, 1007, 90),
+(68, 1008, 90),
+(69, 1009, 85);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `siswa`
+-- Stand-in structure for view `profil siswa`
+-- (See below for the actual view)
+--
+CREATE TABLE `profil siswa` (
+`idsiswa` int(11)
+,`nis` int(30)
+,`nama_siswa` varchar(30)
+,`tempat_lahir` varchar(30)
+,`jenis_kelamin` char(1)
+,`semester` varchar(10)
+,`tahun_ajaran` varchar(10)
+,`alamat_siswa` varchar(200)
+,`nama_kelas` varchar(50)
+,`nama_ayah` varchar(30)
+,`nama_ibu` varchar(30)
+,`nilai` int(3)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
 --
 
 CREATE TABLE `siswa` (
@@ -158,7 +180,7 @@ CREATE TABLE `siswa` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `siswa`
+-- Dumping data for table `siswa`
 --
 
 INSERT INTO `siswa` (`idsiswa`, `nis`, `nama_siswa`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `idkelas`, `semester`, `tahun_ajaran`, `alamat_siswa`, `nama_ayah`, `nama_ibu`) VALUES
@@ -176,7 +198,7 @@ INSERT INTO `siswa` (`idsiswa`, `nis`, `nama_siswa`, `tempat_lahir`, `tanggal_la
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -188,85 +210,94 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id_user`, `nama`, `username`, `password`, `level_user`) VALUES
 (1, 'Mochamad amran al fariz', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
 (2, 'Regha', 'member', 'aa08769cdcb26674c6706093503ff0a3', 'member');
 
+-- --------------------------------------------------------
+
+--
+-- Structure for view `profil siswa`
+--
+DROP TABLE IF EXISTS `profil siswa`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `profil siswa`  AS  select `siswa`.`idsiswa` AS `idsiswa`,`siswa`.`nis` AS `nis`,`siswa`.`nama_siswa` AS `nama_siswa`,`siswa`.`tempat_lahir` AS `tempat_lahir`,`siswa`.`jenis_kelamin` AS `jenis_kelamin`,`siswa`.`semester` AS `semester`,`siswa`.`tahun_ajaran` AS `tahun_ajaran`,`siswa`.`alamat_siswa` AS `alamat_siswa`,`kelas`.`nama_kelas` AS `nama_kelas`,`siswa`.`nama_ayah` AS `nama_ayah`,`siswa`.`nama_ibu` AS `nama_ibu`,`nilai`.`nilai` AS `nilai` from ((`siswa` join `kelas` on((`kelas`.`idkelas` = `siswa`.`idkelas`))) join `nilai` on((`nilai`.`idsiswa` = `siswa`.`idsiswa`))) ;
+
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `guru`
+-- Indexes for table `guru`
 --
 ALTER TABLE `guru`
   ADD PRIMARY KEY (`idguru`);
 
 --
--- Indeks untuk tabel `kelas`
+-- Indexes for table `kelas`
 --
 ALTER TABLE `kelas`
   ADD PRIMARY KEY (`idkelas`);
 
 --
--- Indeks untuk tabel `mata pelajaran`
+-- Indexes for table `mapel`
 --
-ALTER TABLE `mata pelajaran`
+ALTER TABLE `mapel`
   ADD PRIMARY KEY (`idmatapelajaran`),
   ADD KEY `idguru` (`idguru`),
   ADD KEY `idnilai` (`idnilai`);
 
 --
--- Indeks untuk tabel `nilai`
+-- Indexes for table `nilai`
 --
 ALTER TABLE `nilai`
   ADD PRIMARY KEY (`idnilai`),
   ADD KEY `idsiswa` (`idsiswa`);
 
 --
--- Indeks untuk tabel `siswa`
+-- Indexes for table `siswa`
 --
 ALTER TABLE `siswa`
   ADD PRIMARY KEY (`idsiswa`),
   ADD KEY `idkelas` (`idkelas`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `mata pelajaran`
+-- Constraints for table `mapel`
 --
-ALTER TABLE `mata pelajaran`
-  ADD CONSTRAINT `mata pelajaran_ibfk_1` FOREIGN KEY (`idguru`) REFERENCES `guru` (`idguru`);
+ALTER TABLE `mapel`
+  ADD CONSTRAINT `mapel_ibfk_1` FOREIGN KEY (`idguru`) REFERENCES `guru` (`idguru`);
 
 --
--- Ketidakleluasaan untuk tabel `nilai`
+-- Constraints for table `nilai`
 --
 ALTER TABLE `nilai`
   ADD CONSTRAINT `nilai_ibfk_1` FOREIGN KEY (`idsiswa`) REFERENCES `siswa` (`idsiswa`);
 
 --
--- Ketidakleluasaan untuk tabel `siswa`
+-- Constraints for table `siswa`
 --
 ALTER TABLE `siswa`
   ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`idkelas`) REFERENCES `kelas` (`idkelas`);
